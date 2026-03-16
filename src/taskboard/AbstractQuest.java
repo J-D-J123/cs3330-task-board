c4d94e6420edc3caa180a38272d3393edd91e2d7
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2026-03-16 13:20:01 -0500

    added AbstractQuest.java >> default file >> this is fun

---
+++ src/taskboard/AbstractQuest.java
@@ -0,0 +1,21 @@
+package taskboard;
+
+/**
+ * Common fields (id, title, basepoints, completed)
+ * Common validation (fail fast)
+ * Common getters (no fail checks)
+ * A helpful toString()
+ * 
+ * Requirment 
+ * 	- id > 0 
+ * 	- title is not null or blank  
+ * 	- basePoints > 0 
+ * 	
+ * Requried behavior: 
+ * 	- A constructor that validates invariatns (throw IllegalArgumentException)
+ *  - Abstract classes and abstract methods are part of what we covered idk why it says that lol
+ */
+
+public class AbstractQuest {
+
+}
--- src/taskboard/Quest.java
+++ src/taskboard/Quest.java
@@ -1,5 +1,18 @@
 package taskboard;
 
+/**
+ * int getId()
+ * String getTitle()
+ * int getBasePoints()
+ * boolean isCompleted()
+ * int compeleteFor(Student s)
+ * 
+ * Critical Rule: 
+ * 	- Must update the quest's completion state
+ *  - award points to the student (directly or indirectly)
+ *  - and return the number of points awarded 
+ */
+
 public interface Quest {
 
 }
