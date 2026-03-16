d3d56893235706aa25134aadcce8f2690249a81c
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2026-03-16 13:45:49 -0500

    Quest interface with functions
    getId(), getTitle(), getBasePoints(), isCompleted(), and completeFor(Student s)

--- src/taskboard/Quest.java
+++ src/taskboard/Quest.java
@@ -15,4 +15,35 @@ package taskboard;
 
 public interface Quest {
 
-}
+	/**
+	 * getId() gets the id of this
+	 * @return a int 
+	 */
+	int getId(); 
+	
+	/**
+	 * getTitle() gets the title of this
+	 * @return String
+	 */
+	String getTitle();
+	
+	/**
+	 * getBasePoints() returns the amount of basePoints for this
+	 * @return a String
+	 */
+	int getBasePoints();
+	
+	/**
+	 * isCompleted() determins if the status of the quest is completed 
+	 * @return true if completed if not then false 
+	 */
+	boolean isCompleted();
+	
+	/**
+	 * completeFor(Student s) compltes the quest for the specific student s
+	 * @param s specific student doing the quest
+	 * @return the number of points awarded to the student 
+	 */
+	int compeleteFor(Student s);
+	
+}	
