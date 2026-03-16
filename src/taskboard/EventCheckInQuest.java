efcae21ed119a51028e1702d53421583e7d9bd28
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2026-03-16 13:31:34 -0500

    EventCheckInQuest + VolunteerQuest files

---
+++ src/taskboard/EventCheckInQuest.java
@@ -0,0 +1,13 @@
+package taskboard;
+
+/**
+ * A student completes this by checking in at an event
+ * Required data: 
+ * 		- String eventName (not null/ blank)
+ * Completion rule: 
+ * 		- awards exactly basePoints
+ */
+
+public class EventCheckInQuest {
+
+}
---
+++ src/taskboard/VolunteerQuest.java
@@ -0,0 +1,22 @@
+package taskboard;
+
+/**
+ * A student completes this by volunteering some number of hours 
+ * Required additonal data:
+ * 		- int days (must be positive)
+ * 
+ * Choose one (open design choice)
+ * 		- linear bonus: basePoints + (days * 2)
+ * 		- milestone bonus extra bonus if days >= 5 
+ * 
+ * I will choose milestone bonus because it will motivate the person 
+ * to get more tasks done if they get a extra bonus for more days worked
+ * 
+ * Watch out: 
+ * 		Do not implement these differences using conditionals in the quest board. 
+ * 			put the behavior inside the quest classes (polymorphism) 
+ */
+
+public class VolunteerQuest {
+
+}
