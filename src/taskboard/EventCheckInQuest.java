a5a45f64e3813a1cf97b6211e57936660554cb1f
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2026-03-16 19:56:06 -0500

    EventCheckInQuest.java Consturctor
    plus the calcualte points Override based on the AbstractQuest

--- src/taskboard/EventCheckInQuest.java
+++ src/taskboard/EventCheckInQuest.java
@@ -12,6 +12,39 @@ package taskboard;
  * 		- awards exactly basePoints
  */
 
-public class EventCheckInQuest {
+public class EventCheckInQuest extends AbstractQuest {
 
+	private String event_name; 
+	
+	/**
+	 * EventCheckInQuest(...) checks in the 
+	 * @param id is the id of the quest
+	 * @param title the title of the quest
+	 * @param basepoints the amount of points it is
+	 * @param eventName the event title
+	 */
+	public EventCheckInQuest(int id, String title, int basepoints, String eventName) {
+	
+		// call AbstractQuest consturctor to set up Quest 
+		super(id, title, basepoints); 
+		
+		// check if eventName is null or blank if so throw an Illegal Argument 
+		if (eventName == null || eventName.isBlank()) {
+			
+			throw new IllegalArgumentException("eventName cannot be null or blank :)");
+		}
+		
+		event_name = eventName; 
+	}
+	
+	@Override 
+	/**
+	 * calculatePoints(Student s) 
+	 * @param s is the specific student that finished the Task
+	 * @return int of the amount of points that Student got
+	 */
+	protected int calculatePoints(Student s) {
+		
+		return getBasePoints(); 
+	}
 }
