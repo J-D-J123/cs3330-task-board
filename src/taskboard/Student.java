de3e03b5674b39e1701e301449376db2b050d487
Tucker <151984953+TuckerPMIZ@users.noreply.github.com>
2026-03-20 13:41:30 -0500

    more comments and added getPoints().

--- src/taskboard/Student.java
+++ src/taskboard/Student.java
@@ -7,26 +7,37 @@ import java.util.Objects;
 public class Student {
 	private String name;
 	private int points = 0;// need to be > 0
-	
+
+	/*
+	* constructor to get the name of the student.
+	*/
 	public Student(String name) {
 		if(name == null || name.isBlank()) {//enforcing invariants here
 			throw new IllegalArgumentException("You're going to have to re-enter that name");
 		}
 		this.name = name;
 	}
-	
+	/*
+	* This method adds points to the student. The points can't be negative or zero.
+	*/
 	public void addPoints(int amount) {
 		if(amount <= 0) {//need to enforce fail fast validation
 			throw new IllegalArgumentException("We're trying to ADD points here!");
 		}
 		points += amount;
 	}
+	/*
+	* Pretty simple, just a getter for the points of the students
+	*/
+	public int getPoints(){
+		return points;
+	}
 	
 	@Override
 	public String toString() {//toString to list student name and points
 		return "Student name:" + name + ", student points:" + points;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(name);
